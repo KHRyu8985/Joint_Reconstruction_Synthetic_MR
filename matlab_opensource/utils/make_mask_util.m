df4f58ec7873a7fc249279ef670a6577b3fd7a3f
KHRyu8985 <59757365+KHRyu8985@users.noreply.github.com>
2020-04-15 23:34:24 -0700

    Delete make_mask_util.m

--- matlab_opensource/utils/make_mask_util.m
+++
@@ -1,9 +0,0 @@
-function mask = make_mask_util(True_k, pat, nacs)
-    [feRes, peRes, zRes, cRes, dRes, eRes] = size(True_k);
-    ACS = True_k(:,floor(peRes/2)+1+ceil(-nacs/2):floor(peRes/2)+ceil(nacs/2),:,:,:,:);
-    mask = pat;
-    mask(:,floor(peRes/2)+1+ceil(-nacs/2):floor(peRes/2)+ceil(nacs/2),:,:,:,:) = ACS(:,:,:,:,:,:);
-    mask(abs(mask)>0) = 1;
-    
-    mask = single(mask);
-end
