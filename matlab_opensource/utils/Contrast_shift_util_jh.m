2ba5eca2f0ac99ad9292addbe080dfc48b8c30d9
KHRyu8985 <59757365+KHRyu8985@users.noreply.github.com>
2020-04-15 23:33:16 -0700

    Delete Contrast_shift_util_jh.m

--- matlab_opensource/utils/Contrast_shift_util_jh.m
+++
@@ -1,9 +0,0 @@
-function [shift_fe, shift_pe] = Contrast_shift_util_jh(del_fe, del_pe, cRes)
-    shift_fe = repmat(del_fe, [cRes,1]);
-    shift_fe = shift_fe(:).';
-    shift_fe = repmat(shift_fe,1,2);
-
-    shift_pe = repmat(del_pe, [cRes,1]);
-    shift_pe = shift_pe(:).';
-    shift_pe = repmat(shift_pe,1,2);
-end
